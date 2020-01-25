6debca827ad2041604e633c54e072b1567fa2450
Pavel Bondarchuk <bonddesign85@gmail.com>
2020-01-25 23:39:17 +0200

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.c0e3e3c92afddaf3030f4079c15a28ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e142e4b32bab7df46d5826613b5909",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2cb075f80a6458eefd75",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cb075f80a6458eefd75",
+    "url": "/react-gh-pages/static/js/main.e98fe0ac.chunk.js"
+  },
+  {
+    "revision": "ecfd1d276f9241e07c49",
+    "url": "/react-gh-pages/static/js/runtime-main.d24436ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
